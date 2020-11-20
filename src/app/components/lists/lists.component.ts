3f46e7d3be10cc2a95de89679e74f14fe64d0642
Uriel Aispuro <urielalexis64@gmail.com>
2020-11-19 21:57:47 -0700

    editList, ViewChild to manipulate ionic components

--- src/app/components/lists/lists.component.ts
+++ src/app/components/lists/lists.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
 import {TasksService} from '../../services/tasks.service';
 import {ToDoList} from '../../models/todolist.model';
 import {Router} from '@angular/router';
+import {AlertController, IonList} from '@ionic/angular';
 
 @Component({
 	selector: 'app-lists',
@@ -9,12 +10,14 @@ import {Router} from '@angular/router';
 	styleUrls: ['./lists.component.scss'],
 })
 export class ListsComponent {
-	lists: ToDoList[];
+	@ViewChild(IonList) ionList: IonList;
 	@Input() done: boolean;
 
-	constructor(private tasksService: TasksService, private router: Router) {
-		this.lists = tasksService.lists;
-	}
+	constructor(
+		public tasksService: TasksService,
+		private router: Router,
+		private alertController: AlertController
+	) {}
 
 	openList(id) {
 		if (this.done) {
@@ -22,6 +25,59 @@ export class ListsComponent {
 			return;
 		}
 		this.router.navigateByUrl(`tabs/tab1/add/${id}`);
+	}
+
+	async editList(list: ToDoList) {
+		const alert = await this.alertController.create({
+			header: 'Edit title',
+			inputs: [
+				{
+					name: 'title',
+					type: 'text',
+					value: list.title,
+				},
+			],
+			buttons: [
+				{
+					text: 'Cancel',
+					role: 'cancel',
+					handler: () => {
+						this.ionList.closeSlidingItems();
+					},
+				},
+				{
+					text: 'OK',
+					handler: (data) => {
+						const newListTitle = data.title.trim();
+						if (newListTitle.length === 0) {
+							return;
+						}
+						const repeatedName = this.tasksService.lists.filter(
+							(list) => list.title === newListTitle
+						).length;
+						if (repeatedName !== 1) {
+							this.tasksService.editList(list, newListTitle);
+						} else {
+							this.presentAlert('Hey!', 'You can not add a repeated title...');
+						}
+						this.ionList.closeSlidingItems();
+					},
+				},
+			],
+		});
+
+		alert.present();
+	}
+
+	private async presentAlert(header: string, message: string) {
+		const alert = await this.alertController.create({
+			cssClass: 'my-custom-class',
+			header,
+			message,
+			buttons: ['OK'],
+		});
+
+		await alert.present();
 	}
 
 	removeList(list: ToDoList) {
