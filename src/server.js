22f1a7d3de529f737b3e811586066665580ea8fc
Uriel Aispuro <urielalexis64@gmail.com>
2020-11-19 22:42:36 -0700

    heroku deploy

---
+++ src/server.js
@@ -0,0 +1,168 @@
+{
+    "props": {
+        "pageProps": {}
+    },
+    "page": "/embed",
+    "query": {},
+    "buildId": "Bt4jSP0vAviZv5trbCVZo",
+    "runtimeConfig": {},
+    "nextExport": true,
+    "autoExport": true,
+    "isFallback": false,
+    "head": [
+        [
+            "meta",
+            {
+                "name": "viewport",
+                "content": "width=device-width"
+            }
+        ],
+        [
+            "meta",
+            {
+                "charSet": "utf-8"
+            }
+        ],
+        [
+            "meta",
+            {
+                "httpEquiv": "X-UA-Compatible",
+                "content": "IE=edge"
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "description",
+                "content": "Carbon is the easiest way to create and share beautiful images of your source code."
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "application-name",
+                "content": "Carbon"
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "twitter:card",
+                "content": "summary"
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "twitter:site",
+                "content": "@carbon_app"
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "twitter:title",
+                "content": "Carbon"
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "twitter:description",
+                "content": "Carbon is the easiest way to create and share beautiful images of your source code."
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "twitter:image",
+                "content": "https://carbon.now.sh/static/brand/banner.png"
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "og:title",
+                "content": "Carbon"
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "og:description",
+                "content": "Carbon is the easiest way to create and share beautiful images of your source code."
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "og:image",
+                "content": "/static/brand/banner.png"
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "theme-color",
+                "content": "#121212"
+            }
+        ],
+        [
+            "title",
+            {
+                "children": "Carbon | Create and share beautiful images of your source code"
+            }
+        ],
+        [
+            "link",
+            {
+                "rel": "shortcut icon",
+                "href": "/favicon.ico"
+            }
+        ],
+        [
+            "link",
+            {
+                "rel": "manifest",
+                "href": "/manifest.json"
+            }
+        ],
+        [
+            "link",
+            {
+                "rel": "apple-touch-icon",
+                "href": "/static/brand/apple-touch-icon.png"
+            }
+        ],
+        [
+            "link",
+            {
+                "rel": "preload",
+                "as": "style",
+                "href": "//cdnjs.cloudflare.com/ajax/libs/codemirror/5.58.2/theme/seti.min.css"
+            }
+        ],
+        [
+            "link",
+            {
+                "rel": "stylesheet",
+                "href": "//cdnjs.cloudflare.com/ajax/libs/codemirror/5.58.2/theme/seti.min.css"
+            }
+        ],
+        [
+            "link",
+            {
+                "rel": "preload",
+                "as": "style",
+                "href": "//cdnjs.cloudflare.com/ajax/libs/codemirror/5.58.2/codemirror.min.css"
+            }
+        ],
+        [
+            "link",
+            {
+                "rel": "stylesheet",
+                "href": "//cdnjs.cloudflare.com/ajax/libs/codemirror/5.58.2/codemirror.min.css"
+            }
+        ]
+    ]
+}
